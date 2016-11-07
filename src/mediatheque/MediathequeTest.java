a852fb3ecc8677235adfe438e72177ed03820c19
nbarbaste <nicolas.barbaste@edu.ece.fr>
2016-11-07 01:00:16 +0100

    Enprumtable/Consultable

--- src/mediatheque/MediathequeTest.java
+++ src/mediatheque/MediathequeTest.java
@@ -400,13 +400,44 @@ public class MediathequeTest {
 	}
 
 	@Test
-	public void testMetEmpruntable() {
-		fail("Not yet implemented");
+	public void testMetEmpruntableOK() throws OperationImpossible, InvariantBroken {
+		Mediatheque med1 = new Mediatheque(nom);
+		
+		med1.ajouterGenre(au.getGenre().getNom());
+		med1.ajouterLocalisation(au.getLocalisation().getSalle(), au.getLocalisation().getRayon());
+		med1.ajouterDocument(au);
+		
+		med1.metEmpruntable(au.getCode());
+		
+		assertTrue(med1.chercherDocument(au.getCode()).estEmpruntable());
+	}
+	
+	@Test(expected=OperationImpossible.class)
+	public void testMetEmpruntableDocNull() throws OperationImpossible, InvariantBroken {
+		Mediatheque med1 = new Mediatheque(nom);
+		
+		med1.metEmpruntable(au.getCode());
 	}
 
 	@Test
-	public void testMetConsultable() {
-		fail("Not yet implemented");
+	public void testMetConsultableOK() throws OperationImpossible, InvariantBroken {
+		Mediatheque med1 = new Mediatheque(nom);
+		
+		med1.ajouterGenre(au.getGenre().getNom());
+		med1.ajouterLocalisation(au.getLocalisation().getSalle(), au.getLocalisation().getRayon());
+		med1.ajouterDocument(au);
+		
+		med1.metConsultable(au.getCode());
+		
+		//assertTrue(med1.chercherDocument(au.getCode()).estConsultable());
+		// manque le getteur de consultable...
+	}
+	
+	@Test(expected=OperationImpossible.class)
+	public void testMetConsultableDocNull() throws OperationImpossible, InvariantBroken {
+		Mediatheque med1 = new Mediatheque(nom);
+
+		med1.metConsultable(au.getCode());
 	}
 
 	@Test
